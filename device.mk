ee7cb6a36539df29fe8dd58ccfb6bc52e126e3d0
xiaoxi <103127186+xiaoxigg@users.noreply.github.com>
2023-04-14 16:19:37 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6768
 #PRODUCT_STATIC_BOOT_CONTROL_HAL := \更改以下
-PRODUCT_BOOT_CONTROL_HAL := \
+PRODUCT_BOOT_CONTROL_HAL_LIBS := \
     bootctrl.mt6768 \
     libgptutils \
     libz \
